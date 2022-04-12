713ca78262045bc602ee5de0f01d82c7a8d89868
duvalier tsagmo <edtsagmo01@monccnb.ca>
2022-04-12 12:07:06 -0300

    fonctionnemment du servo moteur

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,6 @@
   Version    : 0.0.1
 */
 
-
 #include <Arduino.h>
 #include <Servo.h>
 #include <Wire.h>
@@ -20,55 +19,57 @@ void receiveEvent(int byte);
 
 char boutton;
 
-void setup() {
-  // put your setup code here, to run once:
+void setup()
+{
+    // put your setup code here, to run once:
     Serial.begin(9600);
 
-  wifiConnect();                  //Branchement au réseau WIFI
-  MQTTConnect();                  //Branchement au broker MQTT
+    wifiConnect(); // Branchement au réseau WIFI
+    MQTTConnect(); // Branchement au broker MQTT
 
-    Wire.begin(7);      // Démarrage du périphérique en tant que périphérique.
+    Wire.begin(7); // Démarrage du périphérique en tant que périphérique.
     Wire.onReceive(receiveEvent);
-   moteur1.attach(PIN_SERVO);
+    moteur1.attach(PIN_SERVO);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  	moteur1.write(Angle);     
-
+void loop()
+{
+    // put your main code here, to run repeatedly:
+    moteur1.write(Angle);
 }
 
-void receiveEvent(int byte){
-	boutton = Wire.read();    // read one character from the I2C
+void receiveEvent(int byte)
+{
+    boutton = Wire.read(); // read one character from the I2C
 
-	Serial.print("\nValeur obtenue est : ");
-	Serial.println(boutton);
-	Serial.print("Angle : ");
-	Serial.println(Angle);
-	switch (boutton)
-	{
-	case 'A':
-		Angle = 180;
-		break;
-	case 'B':
-			if (Angle == 180)
-				Angle = 180;
-			else
-				Angle = Angle + 1;
-		break;
-	case 'C':
-		if (Angle == 0)
-				Angle = 0;
-			else
-				Angle = Angle - 1;
-		break;
-	case 'D':
-		Angle = 0;
-		break;
-	
-	default:
-		break;
-	}
+    Serial.print("\nValeur obtenue est : ");
+    Serial.println(boutton);
+    Serial.print("Angle : ");
+    Serial.println(Angle);
+    switch (boutton)
+    {
+    case 'A':
+        Angle = 180;
+        break;
+    case 'B':
+        if (Angle == 180)
+            Angle = 180;
+        else
+            Angle = Angle + 1;
+        break;
+    case 'C':
+        if (Angle == 0)
+            Angle = 0;
+        else
+            Angle = Angle - 1;
+        break;
+    case 'D':
+        Angle = 0;
+        break;
+
+    default:
+        break;
+    }
     appendPayload("Angle", Angle);
     sendPayload();
 }
